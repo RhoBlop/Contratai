630b70987db84b5dde9a9a1bc1a88528c30614d8
Thiago Neves Luz <nevesluzthiago@gmail.com>
2022-08-21 22:54:52 -0300

    Iniciando desenvolvimento da Home Page
    Início do Header (alguns ajustes a serem feitos)

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contrataí</title>
+
+    <!-- SVG FAVICON -->
+    <link rel="icon" href="images/logo/favicon.svg" sizes="any" type="image/svg+xml">
+
+    <!-- CSS BOOTSTRAP -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <?php include ("components/home-header.html"); ?>
+
+    <!-- JS BOOTSTRAP BUNDLE -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
