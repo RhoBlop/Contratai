e61066bcc39f9eed90e38e98999193cc95561bb5
RafaBaker <rafael1309mt@gmail.com>
2022-09-04 18:32:48 -0300

    dropdown funfando

--- home.php
+++ home.php
@@ -2,9 +2,16 @@
 <html lang="en">
     <head>
         <?php include ("components/head.html") ?>
-        <script src="js/isAuthenticated.js"></script>
+        <!--<script src="js/isAuthenticated.js"></script>  -->
     </head>
     <body>
         <?php include ("components/login-header.html") ?>
     </body>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+      crossorigin="anonymous"
+    ></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </html>
