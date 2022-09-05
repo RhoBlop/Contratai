99b259ea7575284ba7afeb237dc6493e8528a457
Rafael Rodrigues <84093736+RafaBaker@users.noreply.github.com>
2022-09-05 12:09:04 -0300

    Tela de perfil pronta
    falta apenas ajeitar o link ativo e centralizar a imagem

--- perfil.php
+++ perfil.php
@@ -9,9 +9,9 @@
 
         <main>
             <div class="container">
-                <div class="row">
-                    <ul class="navbar-nav flex-column col-3 my-3 mx-0">
-                        <li class="nav-item pe-3">
+                <div class="row gx-5">
+                    <ul class="navbar-nav flex-column col-2 my-5 mx-0" id="side-bar">
+                        <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-user fa-fw pe-3"></i>Meu perfil</a>
                         </li>
                         <li class="nav-item">
@@ -26,7 +26,47 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#"><i class="fa-solid fa-screwdriver-wrench fa-fw pe-3"></i>Admin</a>
                         </li>
+                        <li class="nav-item text-danger">
+                            <a class="nav-link" href="#"><i class="fa-solid fa-trash fa-fw pe-3"></i>Excluir Conta</a>
+                        </li>
                     </ul>
+
+                    <div class="flex-column col-7 my-5" id="profile-content">
+                        <div class="mb-5">
+                            <h2>Meu Perfil</h2>
+                            <h6 class="text-muted">Dados da conta</h6>
+                        </div>
+
+                        <img src="https://github.com/mdo.png" alt="" class="rounded-circle mb-5" height="214" width="214">
+
+                        <table class="table mb-5">
+                            <tbody>
+                                <tr>
+                                    <td>Nome completo</td>
+                                    <td class="text-muted">Rafael Rodrigues</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Email</td>
+                                    <td class="text-muted">rafael1309mt@gmail.com</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Data de Nascimento</td>
+                                    <td class="text-muted">13/09/2004</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Telefone</td>
+                                    <td class="text-muted">(27) *****-0259</td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+
+                        <a href="editar.php" class="btn btn-outline-dark">Editar Perfil</a>
+
+                    </div>
                 </div>
             </div>
         </main>
