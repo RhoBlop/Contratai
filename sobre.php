ce7f1ee2844d05e7417b69c16759ad5e8612e8c6
RafaBaker <rafael1309mt@gmail.com>
2022-09-10 02:11:39 -0300

    inicio sobre

---
+++ sobre.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <?php include ("components/head.html") ?>
+  </head>
+  <body>
+    <!-- HOME PAGE HEADER -->
+    <?php include ("components/home-header.html"); ?>
+
+    <!-- MODAL -->
+    <?php include ("components/login-modal.html"); ?>
+
+    <main class="min-vh-100">
+      
+    </main>
+    
+
+    <!-- FOOTER -->
+    <?php include ("components/footer.html") ?>
+
+    <!-- JS BOOTSTRAP BUNDLE -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+      crossorigin="anonymous"
+    ></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  </body>
+</html>
+ 
