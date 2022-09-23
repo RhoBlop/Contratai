8681ce6967394276fe1ee584796d71b5c3bb0c25
Thiago Neves Luz <84537567+RhoBlop@users.noreply.github.com>
2022-09-23 18:07:37 -0300

    Inicio top profissoes

--- php/database/Profissao.php
+++ php/database/Profissao.php
@@ -1,38 +1,47 @@
 <?php 
-    class Profissao {
-        protected $table = "profissao";
+    require_once "Database.php";
 
+    
+    class Profissao extends Database {
+        protected $table = "profissao";
+        
         // retorna
         public function selectMaisContratos($limit) {
             try {
                 $sql = <<<SQL
-                    SELECT count(*) AS numProf, avg(notaAvaliacao) AS mediaAvaliacao, dscProf
+                    SELECT count(*) AS numProf, dscProf
+                    FROM (SELECT dscProf
                     FROM profissao AS prof
                     INNER JOIN especializacao AS espec ON (prof.idprof = espec.idprof)
                     INNER JOIN usrEspec AS usres ON (espec.idespec = usres.idespec)
                     INNER JOIN usuario AS usr ON (usres.idusr = usr.idusr)
-                    INNER JOIN contrato AS contrato ON (espec.idespec = contrato.idespec)
-                    INNER JOIN avaliacao as avaliacao ON (contrato.idcontrato = avaliacao.idcontrato)
                     GROUP BY prof.idProf
                     ORDER BY count(*) DESC
-                    LIMIT :limit
+                    LIMIT :limit) as test
                 SQL;
                 
                 $stmt = Database::prepare($sql);
                 $stmt->execute([ ":limit" => $limit ]);
 
-                return $stmt->fetchAll();
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
             } catch(PDOException $e) {
                 echo json_encode([ "resposta" => "Query SQL Falhou: {$e->getMessage()}" ]);
                 exit();
-
+                
                 return [ "action" => false ];
             }
         }
 
         // retorna as X profissões (de acordo com $limit) com maior média de avaliações 
         public function selectMaiorAvaliacao($limit) {
-
+            
         }
     }
-?>
+    $prof = new Profissao();
+    $profissoes = $prof->selectMaisContratos(3);
+
+    foreach($profissoes as $profi) {
+        print_r($profi);
+        echo "<br>";
+    }
+    ?>
