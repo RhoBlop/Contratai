865dc2cd8516c68da7bafb77980cba9d64ab4ec3
Rafael Rodrigues <84093736+RafaBaker@users.noreply.github.com>
2022-10-31 09:41:19 -0300

    Revert "asgduaysdioy"
    This reverts commit e8af61fe36cb3320620bcdfb405706a14f27dbe9.

--- php/database/Usuario.php
+++ php/database/Usuario.php
@@ -5,30 +5,6 @@ class Usuario extends Database
 {
 
     //SECTION - SELECTS
-
-    //seleciona todos os usuários da tabela usuário
-    public function selectBasicInfo() 
-    {
-        try {
-            $sql = <<<SQL
-                    SELECT * 
-                    FROM usuario
-                SQL;
-            
-            $stmt = Database::prepare($sql);
-            $stmt->execute();
-
-            $result = $stmt->fetchAll();
-            return $result;
-        } catch (PDOException $e) {
-            echo json_encode(["resposta" => "Query SQL Falhou: {$e->getMessage()}"]);
-            exit();
-
-            return ["dados" => false];
-        }
-
-    }
-
     // retorna usuário com o id passado por parâmetro
     public function selectBasicInfoById($userId)
     {
@@ -208,26 +184,25 @@ class Usuario extends Database
         }
     }
 
-    public function selectContratosProfissional($idcontratante, $idstatus)
+    public function selectContratosProfissional($idcontratante)
     {
         try {
             $sql = <<<SQL
-                  SELECT contrt.idcontrato, idcontratante, json_agg(diacontrato) AS diascontrato, timecriacaocontrato, timefinalizacaocontrato, descrespec, usr.iduser, nomeuser, imguser
+                  SELECT contrt.idcontrato, idcontratante, json_agg(diacontrato) AS diascontrato, statcontrt.idstatus, timecriacaocontrato, timefinalizacaocontrato, descrespec, usr.iduser, nomeuser, imguser
                   FROM contrato AS contrt
                   INNER JOIN diacontrato AS diacontrt ON (contrt.idcontrato = diacontrt.idcontrato)
                   INNER JOIN statuscontrato AS statcontrt ON (contrt.idStatus = statcontrt.idStatus)
                   INNER JOIN especializacao AS espec ON (contrt.idespec = espec.idespec)
                   INNER JOIN usuario AS usr ON (contrt.idcontratante = usr.iduser)
-                  WHERE (contrt.idcontratado = :idcontratante) AND (contrt.idstatus = :idstatus)
-                  GROUP BY contrt.idcontrato, descrespec, nomeuser, imguser, usr.iduser
+                  WHERE (contrt.idcontratado = :idcontratante)
+                  GROUP BY contrt.idcontrato, statcontrt.idstatus, descrespec, nomeuser, imguser, usr.iduser
               SQL;
 
 
 
             $stmt = Database::prepare($sql);
             $stmt->execute([
-                ":idcontratante" => $idcontratante,
-                ":idstatus" => $idstatus
+                ":idcontratante" => $idcontratante
             ]);
 
             $result = $stmt->fetchAll();
@@ -235,6 +210,11 @@ class Usuario extends Database
             // converte json_agg() [String] para array associativa
             for ($i = 0; $i < count($result); $i++) {
                 $result[$i]["diascontrato"] = json_decode($result[$i]["diascontrato"]);
+            }
+            
+            // ordena as datas do contrato
+            for ($i = 0; $i < count($result); $i++) {
+                sort($result[$i]["diascontrato"]);
             }
 
             return $result;
@@ -246,24 +226,23 @@ class Usuario extends Database
         }
     }
 
-    public function selectContratosContratante($idcontratante, $idstatus)
+    public function selectContratosContratante($idcontratante)
     {
         try {
             $sql = <<<SQL
-                    SELECT contrt.idcontrato, idcontratado, json_agg(diacontrato) AS diascontrato, timecriacaocontrato, timefinalizacaocontrato, descrespec, usr.iduser, nomeuser, imguser
+                    SELECT contrt.idcontrato, idcontratado, json_agg(diacontrato) AS diascontrato, statcontrt.idstatus, timecriacaocontrato, timefinalizacaocontrato, descrespec, usr.iduser, nomeuser, imguser
                     FROM contrato AS contrt
                     INNER JOIN diacontrato AS diacontrt ON (contrt.idcontrato = diacontrt.idcontrato)
                     INNER JOIN statuscontrato AS statcontrt ON (contrt.idStatus = statcontrt.idStatus)
                     INNER JOIN especializacao AS espec ON (contrt.idespec = espec.idespec)
                     INNER JOIN usuario AS usr ON (contrt.idcontratado = usr.iduser)
-                    WHERE (contrt.idcontratante = :idcontratante) AND (contrt.idstatus = :idstatus)
-                    GROUP BY contrt.idcontrato, descrespec, nomeuser, imguser, usr.iduser
+                    WHERE (contrt.idcontratante = :idcontratante)
+                    GROUP BY contrt.idcontrato, statcontrt.idstatus, descrespec, nomeuser, imguser, usr.iduser
                 SQL;
 
             $stmt = Database::prepare($sql);
             $stmt->execute([
-                ":idcontratante" => $idcontratante,
-                ":idstatus" => $idstatus
+                ":idcontratante" => $idcontratante
             ]);
 
             $result = $stmt->fetchAll();
@@ -271,6 +250,11 @@ class Usuario extends Database
             // converte json_agg() [String] para array associativa
             for ($i = 0; $i < count($result); $i++) {
                 $result[$i]["diascontrato"] = json_decode($result[$i]["diascontrato"]);
+            }
+
+            // ordena as datas do contrato
+            for ($i = 0; $i < count($result); $i++) {
+                sort($result[$i]["diascontrato"]);
             }
 
             return $result;
@@ -282,6 +266,51 @@ class Usuario extends Database
         }
     }
 
+    public function selectNotificacoesDropdown($idUser) {
+        try {
+            $sql = <<<SQL
+                    SELECT *
+                    FROM usuario AS usr
+                    INNER JOIN notificacaocontrato as notific ON (usr.iduser = notific.iddestinatario)
+                    INNER JOIN contrato as contrt ON (notific.idcontrato = contrt.idcontrato)
+                    WHERE (notific.iddestinatario = :id) AND (isavaliado = FALSE);
+                SQL;
+
+            $stmt = Database::prepare($sql);
+            $stmt->execute([
+                ":id" => $idUser
+            ]);
+
+            $result = $stmt->fetchAll();
+            return $result;
+        } catch (PDOException $e) {
+            echo json_encode(["resposta" => "Query SQL Falhou: {$e->getMessage()}"]);
+            exit();
+
+            return ["dados" => false];
+        }
+    }
+
+    public function selectCalendario($idUser) {
+        try {
+            $sql = <<<SQL
+
+                SQL;
+
+            $stmt = Database::prepare($sql);
+            $stmt->execute([
+                ":id" => $idUser
+            ]);
+
+            $result = $stmt->fetchAll();
+            return $result;
+        } catch (PDOException $e) {
+            echo json_encode(["resposta" => "Query SQL Falhou: {$e->getMessage()}"]);
+            exit();
+
+            return ["dados" => false];
+        }
+    }
     //!SECTION - SELECTS
 
 
